94c9bdb85046de4f1f7102bf3b0dd816e04063b0
Davi Marques <davibaterabatera@hotmail.com>
2023-01-15 23:04:43 -0300

    Final project

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, imagem
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
 ]
